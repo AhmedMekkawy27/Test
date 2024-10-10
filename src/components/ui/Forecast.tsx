94d3c1c85265856b5d17ad7e948e4b21eb1ca36b
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-10-10 08:41:05 +0300

    forecast

--- src/components/ui/Forecast.tsx
+++ src/components/ui/Forecast.tsx
@@ -8,7 +8,7 @@ interface IProps{
 
 const Forecast = ({date, maxtemp, mintemp, condition, icon}:IProps) => {
   return (
-    <div className="flex items-center gap-6">
+    <div className="flex items-center gap-2 sm:gap-6">
       <div className="min-w-fit w-9 flex justify-center items-center">
         <h3 className="text-center font-semibold">{date}</h3>
       </div>
