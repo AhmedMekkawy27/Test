5c99d3d44df3313878d505c6276c66440a6e5963
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-11-09 15:48:14 +0200

    forecast ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import Forecast from "@/components/ui/Forecast";
 import { conditionTrim, weekdays } from "@/utils/functions";
-import {MouseEvent, useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 
 export default function Home() {
   const [city, setCity] = useState("cairo");
@@ -9,46 +9,51 @@ export default function Home() {
   const [error, setError] = useState<string | null>(null);
 
   async function fetchWeatherData() {
-    try{
-      const weatherData = await fetch(`https://api.weatherapi.com/v1/forecast.json?days=5&key=72048723f49c411e8e795302240810&q=${city}`).then(response => response.json())
-      setWeather(weatherData)
+    try {
+      const weatherData = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?days=3&key=72048723f49c411e8e795302240810&q=${city}`
+      ).then((response) => response.json());
+      setWeather(weatherData);
     } catch (error) {
       setError("Failed to fetch weather data");
     }
   }
   useEffect(() => {
-    fetchWeatherData()
-
-  },[]);
-  console.log(weather)
+    fetchWeatherData();
+  }, []);
+  // console.log(weather);
   return (
-<div className="min-h-screen bg-gradient-to-r from-gray-900 via-purple-900 to-blue-900 flex justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-purple-900 to-blue-900 flex justify-center items-center">
       <div className="bg-gray-800 rounded-lg shadow-lg p-8 max-w-fit ">
         <h1 className="text-3xl text-white text-center mb-6">Weather App</h1>
         <form>
-        <label htmlFor="city" className="text-white block text-start mb-2">Type City Name:</label>
-        <input
-          id="city"
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter city"
-          className="p-2 w-full rounded text-black"
-        />
+          <label htmlFor="city" className="text-white block text-start mb-2">
+            Type City Name:
+          </label>
+          <input
+            id="city"
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter city"
+            className="p-2 w-full rounded text-black"
+          />
 
-        <button
-          onClick={e =>{e.preventDefault(); fetchWeatherData();}}
-          className="bg-purple-600 text-white px-4 py-2 mt-4 rounded w-full hover:bg-purple-500 transition"
-        >
-          Get Weather
-        </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              fetchWeatherData();
+            }}
+            className="bg-purple-600 text-white px-4 py-2 mt-4 rounded w-full hover:bg-purple-500 transition"
+          >
+            Get Weather
+          </button>
         </form>
 
         {error && <p className="text-red-500 mt-4">{error}</p>}
 
-        {weather.current &&(
+        {weather.current && (
           <div className="mt-6 grid grid-cols-2 gap-4 text-white">
-
             <div className="flex flex-col items-center p-3 border-[1px] rounded-lg border-gray-600">
               <h2 className="">{weather.location.name}</h2>
             </div>
@@ -57,21 +62,34 @@ export default function Home() {
             </div>
             <div className="flex flex-col items-center p-3 border-[1px] rounded-lg border-gray-600">
               <img src={weather.current.condition.icon} alt="" />
-              <h2 className="text-lg font-medium">{weather.current.condition.text}</h2>
+              <h2 className="text-lg font-medium">
+                {weather.current.condition.text}
+              </h2>
             </div>
             <div className="flex justify-center items-center p-3 border-[1px] rounded-lg border-gray-600">
-              <p className="text-4xl font-bold">{weather.current.temp_c.toFixed()} °C</p>
+              <p className="text-4xl font-bold">
+                {weather.current.temp_c.toFixed()} °C
+              </p>
             </div>
             <div className="flex justify-center items-center p-3 border-[1px] rounded-lg border-gray-600">
               <p className="text-md">Humidity: {weather.current.humidity}%</p>
             </div>
             <div className="flex justify-center items-center p-3 border-[1px] rounded-lg border-gray-600">
-              <p className="text-md">Wind: {weather.current.wind_kph.toFixed()}kph</p>
+              <p className="text-md">
+                Wind: {weather.current.wind_kph.toFixed()}kph
+              </p>
             </div>
             <h2 className="col-span-2 font-semibold text-xl">5-day Forecast</h2>
-            <div className="col-span-2 flex flex-col items-center p-3 border-[1px] min-w-fit rounded-lg gap-y-4 border-gray-600 text-white">
-              {weather.forecast.forecastday.map((day:any) => (
-                <Forecast key={day.date} date={weekdays(day.date)} maxtemp={day.day.maxtemp_c.toFixed()} mintemp={day.day.mintemp_c.toFixed()} condition={conditionTrim(day.day.condition.text)} icon={day.day.condition.icon}/>
+            <div className="col-span-2 flex flex-col items-start p-3 border-[1px] min-w-fit rounded-lg gap-y-4 border-gray-600 text-white">
+              {weather.forecast.forecastday.map((day: any) => (
+                <Forecast
+                  key={day.date}
+                  date={weekdays(day.date)}
+                  maxtemp={day.day.maxtemp_c.toFixed()}
+                  mintemp={day.day.mintemp_c.toFixed()}
+                  condition={conditionTrim(day.day.condition.text)}
+                  icon={day.day.condition.icon}
+                />
               ))}
             </div>
           </div>
--- src/components/ui/Forecast.tsx
+++ src/components/ui/Forecast.tsx
@@ -1,26 +1,30 @@
-interface IProps{
-  date: string,
-  maxtemp: number,
-  mintemp: number,
-  condition: string,
-  icon: string,
+interface IProps {
+  date: string;
+  maxtemp: number;
+  mintemp: number;
+  condition: string;
+  icon: string;
 }
 
-const Forecast = ({date, maxtemp, mintemp, condition, icon}:IProps) => {
+const Forecast = ({ date, maxtemp, mintemp, condition, icon }: IProps) => {
   return (
     <div className="flex items-center gap-6">
       <div className="min-w-fit w-9 flex items-center">
         <h3 className="text-center font-semibold">{date}</h3>
       </div>
       <div className="min-w-fit w-28 flex items-center justify-center">
-        <p className="text-center font-medium gap-5 flex items-center"><span>{maxtemp}°C</span> <span>{mintemp}°C</span></p>
+        <p className="text-center font-medium gap-5 flex items-center">
+          <span>{maxtemp}°C</span> <span>{mintemp}°C</span>
+        </p>
       </div>
       <div className="flex items-center gap-1 justify-end">
         <img className="w-8" src={icon} alt="icon" />
-        <p className="text-center text-[12px] sm:text-[16px] font-medium">{condition}</p>
+        <p className="text-center text-[12px] sm:text-[16px] font-medium">
+          {condition}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Forecast
+export default Forecast;
